ed57050bf06a87156d83620ec5e59595d432aa8e
gericass <bonjovi1926@yahoo.co.jp>
2020-06-07 17:19:56 +0900

    add: drawer

--- time-secret/src/assets/js/drawer.js
+++ time-secret/src/assets/js/drawer.js
@@ -1,7 +1,14 @@
 const drawerIcon = document.getElementById('drawer-button');
+const drawerNav = document.getElementById('drawer');
+const main = document.getElementsByTagName('body')[0];
+
+let drawerOpened = false;
 
 export default () => {
   drawerIcon.addEventListener('click', () => {
     drawerIcon.classList.toggle('active');
+    drawerOpened = !drawerOpened;
+    drawerNav.setAttribute('aria-expanded', `${drawerOpened}`);
+    main.classList.toggle('drawer-opened');
   });
 };
